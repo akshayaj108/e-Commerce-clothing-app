d6e825728d5aea62c037e2190caeae61a9b0920d
akshayaj108 <akshayjadhao806@gmail.com>
2023-12-07 16:58:32 +0530

    minor styling bug resolve in directory-item background image

--- src/components/directory-item/directory-item.styles.jsx
+++ src/components/directory-item/directory-item.styles.jsx
@@ -1,4 +1,5 @@
 import styled from "styled-components";
+
 export const BackgroundImage = styled.div`
   width: 100%;
   height: 100%;
@@ -6,6 +7,7 @@ export const BackgroundImage = styled.div`
   background-position: center;
   background-image: ${({ imageUrl }) => `url(${imageUrl})`};
 `;
+
 export const Body = styled.div`
   height: 90px;
   padding: 0 25px;
@@ -33,36 +35,34 @@ export const Body = styled.div`
 `;
 
 export const DirectoryItemContainer = styled.div`
-min-width: 30%;
-height: 240px;
-flex: 1 1 auto;
-display: flex;
-align-items: center;
-justify-content: center;
-border: 1px solid black;
-margin: 0 7.5px 15px;
-overflow: hidden;
+  min-width: 30%;
+  height: 240px;
+  flex: 1 1 auto;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  border: 1px solid black;
+  margin: 0 7.5px 15px;
+  overflow: hidden;
 
-&:hover {
-  cursor: pointer;
-
-  & ${BackgroundImage} {
-    transform: scale(1.1);
-    transition: transform 6s cubic-bezier(0.25, 0.45, 0.45, 0.95);
+  &:first-child {
+    margin-right: 7.5px;
   }
 
-  &${Body} {
-    opacity: 0.9;
+  &:last-child {
+    margin-left: 7.5px;
   }
-}
 
-}
+  &:hover {
+    cursor: pointer;
 
-&:first-child {
-  margin-right: 7.5px;
-}
+    ${BackgroundImage} {
+      transform: scale(1.1);
+      transition: transform 6s cubic-bezier(0.25, 0.45, 0.45, 0.95);
+    }
 
-&:last-child {
-  margin-left: 7.5px;
-}
+    ${Body} {
+      opacity: 0.9;
+    }
+  }
 `;
